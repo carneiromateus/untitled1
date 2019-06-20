b08023069a25bd7a05f08111933ea880d369fd20
Vermêi <murilofch@gmail.com>
2019-06-19 21:39:03 -0300

    REMOVE NODEMODULES

--- public/drag.js
+++ public/drag.js
@@ -1,21 +1,39 @@
 let form = new FormData();
+let formD = new FormData();
 
 $('#btnEncrypt').hide();
+$('#btnDecrypt').hide();
+$('#hash').hide();
 
 $('#file').change(function (event) {
-    console.log('Uploadou mané!!',form);
-
     form.append('file', event.target.files[0]);
-
 
     $('#btnEncrypt').show();
     let name = event.target.files[0].name; // para capturar o nome do arquivo com sua extenção
-
-    $('#filename').text(name);
+    $('#filename').text('Criptografar: '+name);
 
 });
 
+$('#decrypt').change(function (event) {
+    formD.append('file', event.target.files[0]);
+    $('#btnDecrypt').show();
+    $('#hash').show();
 
+    let name = event.target.files[0].name; // para capturar o nome do arquivo com sua extenção
+    $('#filenameD').text('Descriptografar: '+name);
+
+});
+
+function download(data,name){
+    var a = document.createElement('a');
+    var url = window.URL.createObjectURL(data);
+    a.href = url;
+    a.download = name;
+    document.body.append(a);
+    a.click();
+    a.remove();
+    window.URL.revokeObjectURL(url);
+}
 $('#btnEncrypt').click(function () {
     $.ajax({
         url: 'http://localhost:4200/encrypt', // Url do lado server que vai receber o arquivo
@@ -23,16 +41,14 @@ $('#btnEncrypt').click(function () {
         cache: false,
         contentType: false,
         processData: false,
-        headers:{
-            "Access-Control-Allow-Origin": '*',
-            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-        },
         type: 'POST',
         headers: {
             'Access-Control-Allow-Origin': '*',
         },
         success: function (data) {
             console.log('acertou Mizeria',data);
+            $('hashencrypt').text('HASH: ' + data.hash);
+            //download(data.file,'encrypted');
         }
     });
 });
@@ -40,10 +56,11 @@ $('#btnEncrypt').click(function () {
 
 $('#btnDecrypt').click(function () {
     console.log('enviou mané!!');
-
+    let hash = $('#hash').value();
+    formD.append('hash',hash);
     $.ajax({
-        url: 'localhost:4200/encrypt', // Url do lado server que vai receber o arquivo
-        data: file,
+        url: 'localhost:4200/decrypt', // Url do lado server que vai receber o arquivo
+        data: formD,
         processData: false,
         contentType: false,
         type: 'POST',
@@ -52,6 +69,14 @@ $('#btnDecrypt').click(function () {
         },
         success: function (data) {
             console.log('acertou Mizeria',data);
+            if(data.check){
+                alert('Hash Válida');
+                //download(data.file,'decrypted');
+
+            }else{
+                alert('Hash inválida');
+            }
+
         }
     });
 });
